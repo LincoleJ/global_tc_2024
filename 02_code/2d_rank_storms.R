08c8658b138476bcf62b4b250af4b3cef054b85b
lj2575 <lj2575@SPH-DJVKQYY3L9-BST.local>
2025-09-16 11:06:50 -0400

    update code to rank storm

--- 02_code/2d_rank_storms.R
+++ 02_code/2d_rank_storms.R
@@ -1,11 +1,13 @@
 # rank the most-exposed storms in calendar year 2024
 rm(list=ls())
+library(dplyr)
 
 # 0. Load data
 source("./02_code/20_setup/01_helper_functions.R")
-admin2_units <- load_adm2_lookup()  
+admin2_units <- load_adm2_who_mapping()
 adm2_key = admin2_units %>%
-  distinct()
+  distinct() %>% 
+  select(shapeID, shapeName, country)
 year = 2024
 storm_track_dat = readRDS(paste0("./01_data/1a_raw/global_hurr_dat/global_storm_winds_", year, ".csv"))
 tc_exposures <- storm_track_dat %>% filter(vmax_sust >= 17.4911)
@@ -17,6 +19,7 @@ hurr_exposures = hurr_exposures %>% mutate(day = as.Date(date_time_max_wind))
 
 adm2_pop = readr::read_csv(paste0("./01_data/1b_intermediate/pop_by_adm2_interpolated/adm2_pop_", 2020, ".csv"))[, -1] 
 colnames(adm2_pop) = c("shapeID", "pop")
+adm2_pop = left_join(adm2_pop, adm2_key, by = c("shapeID" = "shapeID"))
 
 # join population data with storm wind data
 tc_exposures = left_join(tc_exposures, adm2_pop, by = c("ADM2_id" = "shapeID"))
@@ -29,14 +32,18 @@ person_day_tc_exposure <- tc_exposures %>%
   group_by(ADM2_id, storm_id) %>%
   summarise(total_exposure_days = n_distinct(day), # count unique exposure days for each ADM2
             total_population = max(pop), 
+            country = country,
             .groups = "drop") %>% 
-  mutate(total_person_day_exposure = total_exposure_days * total_population) 
+  mutate(total_person_day_exposure = total_exposure_days * total_population) %>%
+  filter(total_person_day_exposure != 0)
 
 # filter NAs
 pday_tc_exp_nas = person_day_tc_exposure[!complete.cases(person_day_tc_exposure), ]
 
 tc_pday_by_storm = person_day_tc_exposure %>% group_by(storm_id) %>%
-  summarise(total_pday = sum(total_person_day_exposure))
+  summarise(total_pday = sum(total_person_day_exposure),
+            countries_affected = paste(unique(country), collapse = ", "),
+            .groups = "drop")
 top10_storms <- tc_pday_by_storm %>%
   filter(!is.na(total_pday)) %>%
   arrange(desc(total_pday)) %>%
@@ -45,7 +52,7 @@ top10_storms <- tc_pday_by_storm %>%
 top10_tc_storms = person_day_tc_exposure %>% 
   filter(storm_id %in% top10_storms$storm_id) %>%
   left_join(adm2_key, by = c("ADM2_id" = "shapeID")) %>%
-  select(storm_id, ctry_code) %>%
+  select(storm_id, country) %>%
   distinct()
 
 person_day_hurr_exposure <- hurr_exposures %>%
@@ -54,10 +61,15 @@ person_day_hurr_exposure <- hurr_exposures %>%
   group_by(ADM2_id, storm_id) %>%
   summarise(total_exposure_days = n_distinct(day),
             total_population = max(pop),
+            country = country,
             .groups = "drop") %>%
-  mutate(total_person_day_exposure = total_exposure_days * total_population) 
+  mutate(total_person_day_exposure = total_exposure_days * total_population) %>%
+  filter(total_person_day_exposure != 0)
+
 hurr_pday_by_storm = person_day_hurr_exposure %>% group_by(storm_id) %>%
-  summarise(total_pday = sum(total_person_day_exposure))
+  summarise(total_pday = sum(total_person_day_exposure),
+            countries_affected = paste(unique(country), collapse = ", "),
+            .groups = "drop")
 top10_storms <- hurr_pday_by_storm %>%
   filter(!is.na(total_pday)) %>%
   arrange(desc(total_pday)) %>%
@@ -73,6 +85,6 @@ top10_hurr_storms = person_day_hurr_exposure %>%
 # Define the output directory
 output_dir <- "./shiny_app/processed_data_v4/"
 
-# Save the summarized storm data
+# Add country affected to the datasets, and save the summarized storm data
 saveRDS(tc_pday_by_storm, file.path(output_dir, "tc_pday_by_storm.rds"))
 saveRDS(hurr_pday_by_storm, file.path(output_dir, "hurr_pday_by_storm.rds"))
